cb7ffe2fbb5249dc0111cd81dbf8eb95ad2b1ee5
Belkis Aslani <129214713+BEKO2210@users.noreply.github.com>
2026-02-19 21:41:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'gehaltsrechner-v2';
+const CACHE_NAME = 'gehaltsrechner-v3';
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon.svg'
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Install: cache all assets
